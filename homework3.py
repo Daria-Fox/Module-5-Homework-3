8fdc61646a5fcaeca31fb4e22796a674dd0d2b78
Daria-Fox <167777808+Daria-Fox@users.noreply.github.com>
2024-08-09 17:42:37 +0300

    Add files via upload

---
+++ homework3.py
@@ -0,0 +1,83 @@
+class House:
+    def __init__(self, name, number_of_floors):
+        self.name = name
+        self.number_of_floors = number_of_floors
+
+    def go_to(self, new_floor):
+        self.new_floor = new_floor
+        if new_floor >= 1 and new_floor < self.number_of_floors:
+            for i in range(1, new_floor + 1):
+                print(i, end=" ")
+        else:
+            print("Такого этажа не существует")
+
+    def __len__(self):
+        return self.number_of_floors
+
+    def __str__(self):
+        return f"Название: {self.name}, кол-во этажей: {self.number_of_floors}"
+
+    def __eq__(self, other):
+        isinstance(other, int)
+        isinstance(other, House)
+        return self.number_of_floors == other.number_of_floors
+
+    def __lt__(self, other):
+        isinstance(other, int)
+        isinstance(other, House)
+        return self.number_of_floors < other.number_of_floors
+
+    def __le__(self, other):
+        isinstance(other, int)
+        isinstance(other, House)
+        return self.number_of_floors <= other.number_of_floors
+
+    def __gt__(self, other):
+        isinstance(other, int)
+        isinstance(other, House)
+        return self.number_of_floors > other.number_of_floors
+
+    def __ge__(self, other):
+        isinstance(other, int)
+        isinstance(other, House)
+        return self.number_of_floors >= other.number_of_floors
+
+    def __ne__(self, other):
+        isinstance(other, int)
+        isinstance(other, House)
+        return self.number_of_floors != other.number_of_floors
+
+    def __add__(self, value):
+        isinstance(value, int)
+        return self.number_of_floors + value
+
+    def __radd__(self, value):
+        isinstance(value, int)
+        return self.number_of_floors + value
+
+    def __iadd__(self, value):
+        isinstance(value, int)
+        return self.number_of_floors + value
+
+
+h1 = House("ЖК Онегин", 20)
+h2 = House("ЖК Квартет", 10)
+
+h1.go_to(14)
+h2.go_to(-1)
+print(h1)
+print(h2)
+print(len(h1))
+print(len(h2))
+print(h1 == h2)
+print(h1 > h2)
+print(h1 >= h2)
+print(h1 < h2)
+print(h1 <= h2)
+print(h1 != h2)
+h1 = h1 + 10
+print(h1)
+h2 += 10
+print(h2)
+h2 = 15 + h2
+print(h2)
